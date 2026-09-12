383e4702e2d3ed1a47e541dfc3e149a30bc445c3
Hala-Alashraf <halaalashrf@gmail.com>
2026-09-12 21:48:25 +0300

    theme color

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 final Color backgroundColor = Color(0xF0EBDD);
-final Color baseColor = Color(0X0E4A4A);
+final Color baseColor = Color(0x0E4A4A);
 final Color baseColor2 = Color(0xC08552);
-final Color fontColor = Color(0X12262A);
-final Color detailsColor = Color(0XE8DCC8);
+final Color fontColor = Color(0x12262A);
+final Color detailsColor = Color(0xE8DCC8);
+final Color foggyGreen = Color(0x7BA89E);
+
